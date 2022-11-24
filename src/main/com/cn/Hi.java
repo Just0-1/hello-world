0a2cb4465a486e7e3951934dd33af4948bb21d09
Jiang <55074710+Just0-1@users.noreply.github.com>
2022-11-24 22:50:48 +0800

    Update Hi.java

--- src/main/com/cn/Hi.java
+++ src/main/com/cn/Hi.java
@@ -3,6 +3,12 @@ package main.com.cn;
 public class Hi {
     public static void main(String[] args) {
         System.out.println("hi git");
+        
+        
     }
+    class Say{
+static void say(){
+    System.out.println("goodby");
+}
 
 }
